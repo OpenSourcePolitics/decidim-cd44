0d81d8dfcb240f3399a33cbb14ad920cc5eb0f85
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-05-23 17:28:21 +0200

    Update proposal serializer

--- lib/decidim/proposals/proposal_serializer.rb
+++ lib/decidim/proposals/proposal_serializer.rb
@@ -5,7 +5,9 @@ module Decidim
     # This class serializes a Proposal so can be exported to CSV, JSON or other
     # formats.
     class ProposalSerializer < Decidim::Exporters::Serializer
+      include Decidim::ApplicationHelper
       include Decidim::ResourceHelper
+      include Decidim::TranslationsHelper
 
       # Public: Initializes the serializer with a proposal.
       def initialize(proposal)
@@ -15,49 +17,86 @@ module Decidim
       # Public: Exports a hash with the serialized data for this proposal.
       def serialize
         {
-          id: @proposal.id,
+          id: proposal.id,
           category: {
-            id: @proposal.category.try(:id),
-            name: @proposal.category.try(:name)
+            id: proposal.category.try(:id),
+            name: proposal.category.try(:name) || empty_translatable
           },
           scope: {
-            id: @proposal.scope.try(:id),
-            name: @proposal.scope.try(:name)
+            id: proposal.scope.try(:id),
+            name: proposal.scope.try(:name) || empty_translatable
+          },
+          participatory_space: {
+            id: proposal.participatory_space.id,
+            url: Decidim::ResourceLocatorPresenter.new(proposal.participatory_space).url
           },
           author_id: author_id,
-          title: @proposal.title,
-          body: @proposal.body,
-          votes: @proposal.proposal_votes_count,
-          comments: @proposal.comments.count,
-          created_at: @proposal.created_at,
-          published_at: @proposal.published_at,
-          hidden: @proposal.hidden?,
+          component: { id: component.id },
+          title: proposal.title,
+          body: proposal.body,
+          address: proposal.address,
+          latitude: proposal.latitude,
+          longitude: proposal.longitude,
+          state: proposal.state.to_s,
+          reference: proposal.reference,
+          answer: ensure_translatable(proposal.answer),
+          supports: proposal.proposal_votes_count,
+          endorsements: {
+            total_count: proposal.endorsements.size,
+            user_endorsements: user_endorsements
+          },
+          comments: proposal.comments_count,
+          attachments: proposal.attachments.size,
+          followers: proposal.follows.size,
+          published_at: proposal.published_at,
           url: url,
-          feature: { id: feature.id },
-          meeting_urls: meetings
+          meeting_urls: meetings,
+          related_proposals: related_proposals,
+          is_amend: proposal.emendation?,
+          original_proposal: {
+            title: proposal&.amendable&.title,
+            url: original_proposal_url
+          }
         }
       end
 
       private
 
       attr_reader :proposal
+      alias resource proposal
 
-      def feature
-        proposal.feature
-      end
-
-      def author_id
-        proposal.decidim_author_id || "Official"
+      def component
+        proposal.component
       end
 
       def meetings
-        @proposal.linked_resources(:meetings, "proposals_from_meeting").map do |meeting|
+        proposal.linked_resources(:meetings, "proposals_from_meeting").map do |meeting|
           Decidim::ResourceLocatorPresenter.new(meeting).url
+        end
+      end
+
+      def related_proposals
+        proposal.linked_resources(:proposals, "copied_from_component").map do |proposal|
+          Decidim::ResourceLocatorPresenter.new(proposal).url
         end
       end
 
       def url
         Decidim::ResourceLocatorPresenter.new(proposal).url
+      end
+
+      def user_endorsements
+        proposal.endorsements.for_listing.map { |identity| identity.normalized_author&.name }
+      end
+
+      def original_proposal_url
+        return unless proposal.emendation? && proposal.amendable.present?
+
+        Decidim::ResourceLocatorPresenter.new(proposal.amendable).url
+      end
+
+      def author_id
+        proposal.decidim_author_id || "Official"
       end
     end
   end
