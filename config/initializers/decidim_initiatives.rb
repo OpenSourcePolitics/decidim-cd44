d9311a4a2670686b5b2eadf9435673018ae7eb00
eliegaboriau <93646702+eliegaboriau@users.noreply.github.com>
2022-06-06 16:42:19 +0200

    Backport/create initiative authorization (#29)
    Co-authored-by: Pauline Bessoles <52420208+paulinebessoles@users.noreply.github.com>
    Co-authored-by: paulinebessoles <pauline.bessoles@opensourcepolitics.eu>

--- config/initializers/decidim_initiatives.rb
+++ config/initializers/decidim_initiatives.rb
@@ -34,6 +34,18 @@ if defined?(Decidim::Initiatives)
     config.timestamp_service = "Decidim::Initiatives::UtcTimestamp"
   end
 
+  Decidim.resource_registry.find(:initiatives_type).actions += ["create"]
+
+  Decidim::Initiatives::Engine.routes do
+    resources :initiatives, param: :slug, only: [:index, :show, :edit, :update], path: "initiatives" do
+      resources :initiative_signatures
+
+      member do
+        get :authorization_create_modal, to: "authorization_create_modals#show"
+      end
+    end
+  end
+
   Decidim::Initiatives::AdminEngine.routes do
     resources :initiatives_settings, only: [:edit, :update], controller: "initiatives_settings"
   end
