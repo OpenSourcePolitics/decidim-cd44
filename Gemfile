2fc4389640e38ee4169a336c1f542fe55ac1f7db
eliegaboriau <93646702+eliegaboriau@users.noreply.github.com>
2022-06-08 09:18:45 +0200

    add captcha (#37)
    Co-authored-by: paulinebessoles <pauline.bessoles@opensourcepolitics.eu>

--- Gemfile
+++ Gemfile
@@ -9,10 +9,9 @@ ruby RUBY_VERSION
 gem "decidim", git: "https://github.com/decidim/decidim.git", branch: DECIDIM_VERSION
 gem "decidim-initiatives", git: "https://github.com/decidim/decidim.git", branch: DECIDIM_VERSION
 
-#
-# gem "acts_as_textcaptcha", "~> 4.5.1"
-# gem "decidim-question_captcha", git: "https://github.com/OpenSourcePolitics/decidim-module-question_captcha.git", branch: "release/0.24-stable"
+gem "acts_as_textcaptcha", "~> 4.5.1"
 gem "decidim-decidim_awesome", "~> 0.8.3"
+gem "decidim-question_captcha", git: "https://github.com/OpenSourcePolitics/decidim-module-question_captcha.git", branch: DECIDIM_VERSION
 gem "decidim-simple_proposal", git: "https://github.com/OpenSourcePolitics/decidim-module-simple_proposal.git", branch: "fix/0.26-proposal-form"
 gem "decidim-spam_detection"
 gem "decidim-term_customizer", git: "https://github.com/mainio/decidim-module-term_customizer.git", branch: "master"
--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -5,75 +5,165 @@ require "spec_helper"
 describe "Authentication", type: :system do
   let(:organization) { create(:organization) }
   let(:last_user) { Decidim::User.last }
+  let(:app_questions) do
+    {
+      en: [{ "question" => "1+1", "answers" => "2" }],
+      fr: [{ "question" => "2+1", "answers" => "3" }]
+    }
+  end
+  let(:api_questions) { nil }
+  let(:api_endpoint) { nil }
+  let(:en_api_questions) do
+    { "q" => "What is the color of the white horse", "a" => [Digest::MD5.hexdigest("white")] }
+  end
+  let(:fr_api_questions) do
+    { "q" => "Quelle est la couleur du cheval gris ?", "a" => [Digest::MD5.hexdigest("gris")] }
+  end
+
+  let(:cache_store) { :memory_store }
 
   before do
+    allow(Rails).to receive(:cache).and_return(ActiveSupport::Cache.lookup_store(cache_store))
+    Rails.cache.clear
+    allow(Decidim::QuestionCaptcha.config).to receive(:questions).and_return(app_questions)
     switch_to_host(organization.host)
     visit decidim.root_path
   end
 
   describe "Sign Up" do
-    context "when using email and password" do
-      it "creates a new User" do
-        find(".sign-up-link").click
+    context "when using app provided questions" do
+      context "when using email and password" do
+        it "creates a new User" do
+          sign_up_user(captcha_answer: "2")
 
-        within ".new_user" do
-          fill_in :registration_user_email, with: "user@example.org"
-          fill_in :registration_user_name, with: "Responsible Citizen"
-          fill_in :registration_user_nickname, with: "responsible"
-          fill_in :registration_user_password, with: "DfyvHn425mYAy2HL"
-          fill_in :registration_user_password_confirmation, with: "DfyvHn425mYAy2HL"
-          check :registration_user_tos_agreement
-          check :registration_user_newsletter
-          find("*[type=submit]").click
+          expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
+        end
+      end
+
+      context "when using another language" do
+        before do
+          within_language_menu do
+            click_link "Français"
+          end
         end
 
-        expect(page).to have_content("A message with a confirmation link has been sent to your email address")
+        it "keeps the locale settings" do
+          sign_up_user(captcha_answer: "3")
+
+          expect(page).to have_content("lien de confirmation")
+          expect(last_user.locale).to eq("fr")
+        end
+      end
+
+      context "when being a robot" do
+        it "denies the sign up" do
+          sign_up_user(captcha_answer: "2", robot: true)
+
+          expect(page).not_to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
+        end
+      end
+
+      context "when captcha is wrong" do
+        it "denies the sign up" do
+          sign_up_user(captcha_answer: "wrong")
+
+          expect(page).not_to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
+        end
+      end
+
+      context "when using :null_store" do
+        let(:cache_store) { :null_store }
+
+        it "doesn't display a captcha field" do
+          find(".sign-up-link").click
+
+          expect(page).not_to have_field(:user_textcaptcha_answer)
+        end
       end
     end
 
-    context "when using another langage" do
+    context "when using api provided questions" do
+      let(:api_endpoint) { "https:://mock-api.org" }
+
       before do
-        within_language_menu do
-          click_link "Français"
+        # rubocop:disable RSpec/AnyInstance
+        allow_any_instance_of(Decidim::RegistrationForm).to receive(:fetch_q_and_a).and_return(api_questions)
+        # rubocop:enable RSpec/AnyInstance
+        allow(Decidim::QuestionCaptcha.config).to receive(:api_endpoint).and_return(api_endpoint)
+      end
+
+      context "when using email and password" do
+        let(:api_questions) { en_api_questions }
+
+        it "creates a new User" do
+          sign_up_user(captcha_answer: "white")
+
+          expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
         end
       end
 
-      it "keeps the locale settings" do
-        find(".sign-up-link").click
+      context "when using another language" do
+        let(:api_questions) { fr_api_questions }
 
-        within ".new_user" do
-          fill_in :registration_user_email, with: "user@example.org"
-          fill_in :registration_user_name, with: "Responsible Citizen"
-          fill_in :registration_user_nickname, with: "responsible"
-          fill_in :registration_user_password, with: "DfyvHn425mYAy2HL"
-          fill_in :registration_user_password_confirmation, with: "DfyvHn425mYAy2HL"
-          check :registration_user_tos_agreement
-          check :registration_user_newsletter
-          find("*[type=submit]").click
+        before do
+          within_language_menu do
+            click_link "Français"
+          end
         end
 
-        expect(page).to have_content("Un message avec un lien de confirmation a été envoyé à votre adresse e-mail")
-        expect(last_user.locale).to eq("fr")
+        it "keeps the locale settings" do
+          sign_up_user(captcha_answer: "gris")
+
+          expect(page).to have_content("lien de confirmation")
+          expect(last_user.locale).to eq("fr")
+        end
       end
-    end
 
-    context "when being a robot" do
-      it "denies the sign up" do
-        find(".sign-up-link").click
+      context "when api response returns no question" do
+        let(:api_questions) { nil }
 
-        within ".new_user" do
-          page.execute_script("$($('.new_user > div > input')[0]).val('Ima robot :D')")
-          fill_in :registration_user_email, with: "user@example.org"
-          fill_in :registration_user_name, with: "Responsible Citizen"
-          fill_in :registration_user_nickname, with: "responsible"
-          fill_in :registration_user_password, with: "DfyvHn425mYAy2HL"
-          fill_in :registration_user_password_confirmation, with: "DfyvHn425mYAy2HL"
-          check :registration_user_tos_agreement
-          check :registration_user_newsletter
-          find("*[type=submit]").click
+        before do
+          within_language_menu do
+            click_link "Français"
+          end
         end
 
-        expect(page).not_to have_content("confirmation link")
+        it "falbacks to app questions" do
+          sign_up_user(captcha_answer: "3")
+
+          expect(page).to have_content("lien de confirmation")
+          expect(last_user.locale).to eq("fr")
+        end
+      end
+
+      context "when being a robot" do
+        let(:api_questions) { en_api_questions }
+
+        it "denies the sign up" do
+          sign_up_user(captcha_answer: "white", robot: true)
+
+          expect(page).not_to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
+        end
+      end
+
+      context "when captcha is wrong" do
+        let(:api_questions) { en_api_questions }
+
+        it "denies the sign up" do
+          sign_up_user(captcha_answer: "wrong")
+
+          expect(page).not_to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
+        end
+      end
+
+      context "when using :null_store" do
+        let(:cache_store) { :null_store }
+
+        it "doesn't display a captcha field" do
+          find(".sign-up-link").click
+
+          expect(page).not_to have_field(:user_textcaptcha_answer)
+        end
       end
     end
 
@@ -583,6 +673,7 @@ describe "Authentication", type: :system do
             fill_in :registration_user_nickname, with: "responsible"
             fill_in :registration_user_password, with: "DfyvHn425mYAy2HL"
             fill_in :registration_user_password_confirmation, with: "DfyvHn425mYAy2HL"
+            fill_in :registration_user_textcaptcha_answer, with: "2"
             check :registration_user_tos_agreement
             check :registration_user_newsletter
             find("*[type=submit]").click
@@ -656,6 +747,25 @@ describe "Authentication", type: :system do
         expect(page).to have_content("successfully")
         expect(page).to have_content("Right user")
       end
+    end
+  end
+
+  private
+
+  def sign_up_user(captcha_answer: nil, robot: false)
+    find(".sign-up-link").click
+
+    within ".new_user" do
+      page.execute_script("$($('.new_user > div > input')[0]).val('Ima robot :D')") if robot
+      fill_in :registration_user_email, with: "user@example.org"
+      fill_in :registration_user_name, with: "Responsible Citizen"
+      fill_in :registration_user_nickname, with: "responsible"
+      fill_in :registration_user_password, with: "DfyvHn425mYAy2HL"
+      fill_in :registration_user_password_confirmation, with: "DfyvHn425mYAy2HL"
+      fill_in :registration_user_textcaptcha_answer, with: captcha_answer
+      check :registration_user_tos_agreement
+      check :registration_user_newsletter
+      find("*[type=submit]").click
     end
   end
 end
