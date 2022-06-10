0bea6e0384d8ca47e5d2279d067f69b9767dbf78
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-06-10 16:02:04 +0200

    Remove rspec failure exit code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,6 @@ DEFAULT_LOCALE = :en
 AVAILABLE_LOCALES = [:en, :fr].freeze
 
 RSpec.configure do |config|
-  config.failure_exit_code = 48
-
   config.before do
     # I18n configuration
     I18n.available_locales = AVAILABLE_LOCALES
