748fda16aa89a4e18c2983147370fec6d9b4a5a4
moustachu <moustachu@opensourcepolitics.eu>
2022-07-29 19:07:19 +0200

    Allow to configure session timeout

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -4,6 +4,10 @@ Decidim.configure do |config|
   config.unconfirmed_access_for = 2.days unless Rails.env.test?
   config.skip_first_login_authorization = ENV["SKIP_FIRST_LOGIN_AUTHORIZATION"] ? ActiveRecord::Type::Boolean.new.cast(ENV["SKIP_FIRST_LOGIN_AUTHORIZATION"]) : true
 
+  if Rails.application.secrets.decidim[:expire_session_after].present?
+    config.expire_session_after = Rails.application.secrets.decidim[:expire_session_after].to_i.seconds
+  end
+
   if Rails.application.secrets.decidim[:session_timeout_interval].present?
     config.session_timeout_interval = Rails.application.secrets.decidim[:session_timeout_interval].to_i.seconds
   end
