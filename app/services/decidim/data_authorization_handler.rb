0cba175bdf94726f9d4ce017df2c47b071396808
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-11-09 16:33:55 +0100

    Rubocop Changes to change line to make it more readable

--- app/services/decidim/data_authorization_handler.rb
+++ app/services/decidim/data_authorization_handler.rb
@@ -12,7 +12,9 @@ module Decidim
     validates :gdpr, acceptance: true
     validates :lastname, presence: true
     validates :firstname, presence: true
-    validates :phone, presence: true, format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/, message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.phone_invalid") }
+    validates :phone, presence: true,
+                      format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/,
+                                message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.phone_invalid") }
     def metadata
       super.merge(firstname: firstname, lastname: lastname, phone: phone, structure: structure)
     end
