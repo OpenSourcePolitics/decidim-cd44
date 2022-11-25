d94cf0f07ea23c05802e615c2472b52cfcd0def4
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-11-25 13:13:32 +0100

    Use normal message from devise for the acceptance, but addin the presence to add the '*' to our checkbox message.

--- app/services/decidim/data_authorization_handler.rb
+++ app/services/decidim/data_authorization_handler.rb
@@ -11,16 +11,15 @@ module Decidim
     attribute :zipcode, String
     attribute :city, String
 
-
     validates :firstname, presence: true, format: { with: /\A[a-zA-ZÀ-ÿ'\-\s]+\z/,
                                                     message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.error_message") }
     validates :lastname, presence: true, format: { with: /\A[a-zA-ZÀ-ÿ'\-\s]+\z/,
                                                    message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.error_message") }
     validates :phone, presence: true
 
-    validates :gdpr, acceptance: { message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.acceptance_error") }
+    validates :gdpr, acceptance: true, presence: { message: "" }
 
-    validates :minimum_age, acceptance: { message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.acceptance_error") }
+    validates :minimum_age, acceptance: true, presence: { message: "" }
 
     validates :zipcode, presence: true, format: { with: /\A[0-9]{5}\z/,
                                                   message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.error_message") }
@@ -28,7 +27,7 @@ module Decidim
     validates :city, presence: { message: I18n.t("decidim.authorization_handlers.data_authorization_handler.errors.no_zipcode_result") }
 
     def metadata
-      super.merge(firstname: firstname, lastname: lastname, phone: phone, zipcode: zipcode, city: city)
+      super.merge(firstname: firstname, lastname: lastname, phone: phone, zipcode: zipcode, city: city, gdpr: gdpr, minimum_age: minimum_age)
     end
   end
 end
