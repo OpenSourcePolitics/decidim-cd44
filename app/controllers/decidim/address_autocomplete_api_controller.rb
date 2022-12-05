c264bc3ea662b42a2b90f5cfb54626e414b919a7
armandfardeau <fardeauarmand@gmail.com>
2022-12-05 15:51:36 +0100

    Move logic to a proper service

--- app/controllers/decidim/address_autocomplete_api_controller.rb
+++ app/controllers/decidim/address_autocomplete_api_controller.rb
@@ -1,45 +1,7 @@
-require "uri"
-require "net/http"
-
 module Decidim
   class AddressAutocompleteApiController < DecidimController
     def postal_code_autocomplete
-      render json: JSON.dump(municipality_names)
-    end
-
-    private
-
-    def municipality_names
-      return Rails.cache.read(cache_key) if Rails.cache.read(cache_key).present?
-
-      Rails.cache.fetch(cache_key, expires_in: 1.months) do
-        municipality_names_request
-      end
-    end
-
-    def municipality_names_request
-      https = Net::HTTP.new(url.host, url.port)
-      https.use_ssl = true
-      request = Net::HTTP::Get.new(url)
-      response = https.request(request)
-
-      features = JSON.parse(response.read_body).fetch("features", [])
-      features.map do |feature|
-        feature.fetch("properties", {}).fetch("name", nil)
-      end
-
-    rescue StandardError => e
-      logger.warn("Error while fetching municipality names for postal code #{params[:postal_code]}. Response code: #{response.code} with error #{e}")
-
-      []
-    end
-
-    def cache_key
-      "postal_code_autocomplete/#{params[:postal_code]}"
-    end
-
-    def url
-      @url ||= URI("https://api-adresse.data.gouv.fr/search/?q=#{params[:postal_code]}&type=municipality")
+      render json: JSON.dump(Decidim::MunicipalityNamesService.for(params[:postal_code]))
     end
   end
 end
---
+++ app/services/decidim/municipality_names_service.rb
@@ -0,0 +1,50 @@
+require "uri"
+require "net/http"
+require 'json'
+
+module Decidim
+  class MunicipalityNamesService
+    def initialize(postal_code)
+      @postal_code = postal_code
+    end
+
+    def self.for(postal_code)
+      new(postal_code).call
+    end
+
+    def call
+      return Rails.cache.read(cache_key) if Rails.cache.read(cache_key).present?
+
+      Rails.cache.fetch(cache_key, expires_in: 1.months) do
+        request
+      end
+    end
+
+    private
+
+    def request
+      https = Net::HTTP.new(url.host, url.port)
+      https.use_ssl = true
+      request = Net::HTTP::Get.new(url)
+      response = https.request(request)
+
+      features = JSON.parse(response.read_body).fetch("features", [])
+      features.map do |feature|
+        feature.fetch("properties", {}).fetch("name", nil)
+      end
+
+    rescue StandardError => e
+      logger.warn("Error while fetching municipality names for postal code #{@postal_code}. Response code: #{response.code} with error #{e}")
+
+      []
+    end
+
+    def cache_key
+      "postal_code_autocomplete/#{@postal_code}"
+    end
+
+    def url
+      @url ||= URI("https://api-adresse.data.gouv.fr/search/?q=#{@postal_code}&type=municipality")
+    end
+  end
+end
