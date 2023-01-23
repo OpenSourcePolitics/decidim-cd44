a42d762e6c6cbc51ac00de14e69344c2981b96b0
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-01-23 17:32:21 +0100

    add rake task

--- lib/tasks/duplicates_metadata.rake
+++ lib/tasks/duplicates_metadata.rake
@@ -5,30 +5,15 @@ namespace :decidim do
     desc "Merge all authorization encrypted metadata to decrypted metadata in user extended data"
     task metadata: :environment do
       logger = Logger.new($stdout)
-      logger.info(logger_output("Retrieving all extended socio demographic authorizations"))
+      logger.info(logger_output("Retrieving all extended socio demographic and data authorizations"))
 
-      authorizations = Decidim::Authorization.where(name: "extended_socio_demographic_authorization_handler")
-      updated = []
-      authorizations.each do |auth|
-        if auth.user.blank? || !auth.user.respond_to?(:extended_data)
-          logger.error(logger_output("Undefined user for authorization ID/#{auth.id}"))
-          next
-        end
-        next if auth.user.extended_data.include?("socio_postal_code")
-        next if auth.user.extended_data.include?("socio_city")
-        next if auth.user.extended_data.include?("socio_email")
-        next if auth.user.extended_data.include?("socio_phone_number")
+      socio_authorizations = Decidim::Authorization.where(name: "extended_socio_demographic_authorization_handler")
+      data_authorizations = Decidim::Authorization.where(name: "data_authorization_handler")
 
-        extended_data = auth.metadata.deep_transform_keys { |key| "socio_#{key}" }
+      updated = update_socio_authorized_users(socio_authorizations) + update_data_authorized_users(data_authorizations)
 
-        if auth.user.update(extended_data: auth.user.extended_data.merge(extended_data))
-          logger.info(logger_output("Updating user (ID/#{auth.user.id})"))
-          updated << auth.user.id
-        else
-          logger.error(logger_output("Errors happened while updating user (ID/#{auth.user.id})"))
-        end
-      end
-      logger.info(logger_output("Found #{authorizations.count} extended socio demographic authorizations"))
+      logger.info(logger_output("Found #{socio_authorizations.count} extended socio demographic authorizations"))
+      logger.info(logger_output("Found #{data_authorizations.count} data authorizations"))
       logger.info(logger_output("Updated #{updated.count} users : #{updated.count.positive? ? updated : "None"}"))
 
       exit 0
@@ -38,4 +23,53 @@ end
 
 def logger_output(msg = "", task_name = "decidim:duplicates:metadata")
   "[#{task_name}] :: #{msg}"
+end
+
+def update_socio_authorized_users(socio_authorizations)
+  updated = []
+  socio_authorizations.each do |auth|
+    if auth.user.blank? || !auth.user.respond_to?(:extended_data)
+      logger.error(logger_output("Undefined user for authorization ID/#{auth.id}"))
+      next
+    end
+
+    next if auth.user.extended_data.include?("extended_socio_demographic_authorization_handler")
+
+    if auth.user.extended_data.include?("socio_postal_code")
+      metadata = {
+        "postal_code" => auth.user.extended_data["socio_postal_code"],
+        "city" => auth.user.extended_data["socio_city"],
+        "email" => auth.user.extended_data["socio_email"],
+        "phone_number" => auth.user.extended_data["socio_phone_number"]
+      }
+      auth.user.update!(extended_data: auth.user.extended_data.reject { |key| key.start_with?("socio_") })
+    end
+    if auth.user.update(extended_data: {"extended_socio_demographic_authorization_handler" => auth.user.extended_data.merge(metadata || auth.metadata)})
+      logger.info(logger_output("Updating user (ID/#{auth.user.id})"))
+      updated << auth.user.id
+    else
+      logger.error(logger_output("Errors happened while updating user (ID/#{auth.user.id})"))
+    end
+  end
+  updated
+end
+
+def update_data_authorized_users(data_authorizations)
+  updated = []
+  data_authorizations.each do |auth|
+    if auth.user.blank? || !auth.user.respond_to?(:extended_data)
+      logger.error(logger_output("Undefined user for authorization ID/#{auth.id}"))
+      next
+    end
+
+    next if auth.user.extended_data.include?("data_authorization_handler")
+
+    if auth.user.update(extended_data: {"data_authorization_handler" => auth.user.extended_data.merge(auth.metadata)})
+      logger.info(logger_output("Updating user (ID/#{auth.user.id})"))
+      updated << auth.user.id
+    else
+      logger.error(logger_output("Errors happened while updating user (ID/#{auth.user.id})"))
+    end
+  end
+  updated
 end
--- spec/lib/tasks/duplicates_metadata_spec.rb
+++ spec/lib/tasks/duplicates_metadata_spec.rb
@@ -7,11 +7,15 @@ describe "rake decidim:duplicates:metadata", type: :task do
   let(:task_cmd) { :"decidim:duplicates:metadata" }
 
   let!(:user) { create(:user, organization: organization) }
-  let(:authorization) { create(:authorization, user: user, metadata: metadata) }
+  let(:socio_authorization) { create(:authorization, user: user, metadata: socio_metadata, name: "extended_socio_demographic_authorization_handler") }
+  let(:data_authorization) { create(:authorization, user: user, metadata: data_metadata, name: "data_authorization_handler") }
   let!(:user2) { create(:user, organization: organization) }
-  let(:authorization_user2) { create(:authorization, user: user2, metadata: metadata_user2) }
-  let(:metadata) { { email: user.email } }
-  let(:metadata_user2) { { email: user2.email } }
+  let(:socio_authorization_user2) { create(:authorization, user: user2, metadata: socio_metadata_user2, name: "extended_socio_demographic_authorization_handler") }
+  let(:data_authorization_user2) { create(:authorization, user: user2, metadata: data_metadata_user2, name: "data_authorization_handler") }
+  let(:socio_metadata) { { email: user.email, postal_code: "12345", city: "Barcelona", phone_number: "01234567889" } }
+  let(:data_metadata) { { minmum_age: true, gdpr: true, zipcode: "12345" } }
+  let(:socio_metadata_user2) { { email: user2.email, postal_code: "54321", city: "Paris", phone_number: "9876543210" } }
+  let(:data_metadata_user2) { { minmum_age: true, gdpr: true, zipcode: "54321" } }
 
   context "when executing task" do
     it "exits with 0 code" do
@@ -19,16 +23,57 @@ describe "rake decidim:duplicates:metadata", type: :task do
         expect(error.status).to eq(0)
       end
     end
+  end
 
+  context "when user has no metadata" do
     it "duplicates encrypted metadata to user registration_metadata" do
-      expect(user.extended_data).to eq({})
-      expect(user2.extended_data).to eq({})
+      expect(user.extended_data["extended_socio_demographic_authorization_handler"]).to be_nil
+      expect(user.extended_data["data_authorization_handler"]).to be_nil
+      expect(user2.extended_data["extended_socio_demographic_authorization_handler"]).to be_nil
+      expect(user2.extended_data["data_authorization_handler"]).to be_nil
       Rake::Task[task_cmd].invoke
 
       user.reload!
       user2.reload!
-      expect(user.extended_data).to eq(metadata)
-      expect(user2.extended_data).to eq(metadata_user2)
+      expect(user.extended_data["extended_socio_demographic_authorization_handler"]).to eq(socio_metadata)
+      expect(user.extended_data["data_authorization_handler"]).to eq(data_metadata)
+      expect(user2.extended_data["extended_socio_demographic_authorization_handler"]).to eq(socio_metadata_user2)
+      expect(user2.extended_data["data_authorization_handler"]).to eq(data_metadata_user2)
+    end
+  end
+
+  context "when user has already metadata" do
+    it "doesn't duplicates the useless metadata and duplicates the good ones" do
+      user.update!(extended_data: { "extended_socio_demographic_authorization_handler" => socio_metadata })
+      user2.update!(extended_data: { "data_authorization_handler" => data_metadata_user2 })
+      Rake::Task[task_cmd].invoke
+      expect(true).to eq(false)
+      user.reload!
+      user2.reload!
+      expect(user.extended_data["extended_socio_demographic_authorization_handler"]).to eq(socio_metadata)
+      expect(user.extended_data["data_authorization_handler"]).to eq(data_metadata)
+      expect(user2.extended_data["extended_socio_demographic_authorization_handler"]).to eq(socio_metadata_user2)
+      expect(user2.extended_data["data_authorization_handler"]).to eq(data_metadata_user2)
+    end
+  end
+
+  context "when user has already metadata for socio but the old format" do
+    it "remove the old format and add the new one" do
+      user.update!(extended_data: {
+        "socio_email" => socio_metadata["email"],
+        "socio_postal_code" => socio_metadata["postal_code"],
+        "socio_city" => socio_metadata["city"],
+        "socio_phone_number" => socio_metadata["phone_number"]
+      })
+
+      Rake::Task[task_cmd].invoke
+
+      user.reload!
+      expect(user.extended_data["extended_socio_demographic_authorization_handler"]).to eq(socio_metadata)
+      expect(user.extended_data).not_to include("socio_email")
+      expect(user.extended_data).not_to include("socio_postal_code")
+      expect(user.extended_data).not_to include("socio_city")
+      expect(user.extended_data).not_to include("socio_phone_number")
     end
   end
 end
