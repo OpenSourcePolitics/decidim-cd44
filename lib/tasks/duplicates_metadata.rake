b6639089cf1500dc69253d98ed8d7187cee73429
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-02-28 19:11:28 +0100

    Refactor specs and rake task

--- lib/tasks/duplicates_metadata.rake
+++ lib/tasks/duplicates_metadata.rake
@@ -6,7 +6,11 @@ namespace :decidim do
   namespace :duplicates do
     desc "Merge all authorization encrypted metadata to decrypted metadata in user extended data"
     task metadata: :environment do
-      Decidim::DuplicatesMetadata.new.perform
+      puts "[decidim:duplicates:metadata] - Duplicating authorizations metadata..."
+      metadata_duplicator = Decidim::DuplicatesMetadata.new
+      puts "[decidim:duplicates:metadata] - Found #{metadata_duplicator.authorizations.count} authorizations..."
+      metadata_duplicator.perform
+      puts "[decidim:duplicates:metadata] - Terminated."
     end
   end
 end
--- spec/lib/decidim/duplicates_metadata_spec.rb
+++ spec/lib/decidim/duplicates_metadata_spec.rb
@@ -14,13 +14,13 @@ describe Decidim::DuplicatesMetadata do
   let!(:socio_authorization) do
     create(:authorization,
            user: user,
-           metadata: { "email" => user.email, "postal_code" => "12345", "city" => "Barcelona", "phone_number" => "01234567889" },
+           metadata: { "email" => "user@example.org", "postal_code" => "12345", "city" => "Barcelona", "phone_number" => "01234567889" },
            name: "extended_socio_demographic_authorization_handler")
   end
   let!(:socio_authorization_user2) do
     create(:authorization,
            user: user2,
-           metadata: { "email" => user2.email, "postal_code" => "54321", "city" => "Paris", "phone_number" => "9876543210" },
+           metadata: { "email" => "user2@example.org", "postal_code" => "54321", "city" => "Paris", "phone_number" => "9876543210" },
            name: "extended_socio_demographic_authorization_handler")
   end
 
@@ -37,14 +37,19 @@ describe Decidim::DuplicatesMetadata do
            name: "data_authorization_handler")
   end
 
-  shared_examples "has duplicated encrypted metadata to user registration_metadata" do
-    before do
+  it "has :authorizations attr_reader" do
+    expect(subject.authorizations.count).to eq(4)
+  end
+
+  describe "#perform" do
+    it "updates users extended_data" do
+      expect(user.extended_data).not_to include("extended_socio_demographic_authorization_handler", "data_authorization_handler")
+      expect(user2.extended_data).not_to include("extended_socio_demographic_authorization_handler", "data_authorization_handler")
+
       subject.perform
       user.reload
       user2.reload
-    end
 
-    it "has duplicated encrypted metadata to user registration_metadata" do
       expect(user.extended_data).to include(
         "extended_socio_demographic_authorization_handler" => socio_authorization.metadata,
         "data_authorization_handler" => data_authorization.metadata
@@ -55,60 +60,69 @@ describe Decidim::DuplicatesMetadata do
         "data_authorization_handler" => data_authorization_user2.metadata
       )
     end
-  end
 
-  context "when user has no metadata" do
-    before do
-      expect(user.extended_data).not_to include("extended_socio_demographic_authorization_handler", "data_authorization_handler")
-      expect(user2.extended_data).not_to include("extended_socio_demographic_authorization_handler", "data_authorization_handler")
+    context "when user already has metadata" do
+      let!(:user) { create(:user, organization: organization, extended_data: user_extended_data) }
+      let(:user_extended_data) do
+        { "extended_socio_demographic_authorization_handler" =>
+            { "email" => "user@example.org", "postal_code" => "12345",
+              "city" => "Barcelona", "phone_number" => "01234567889" } }
+      end
+
+      it "duplicates non existing authorization metadata" do
+        expect(user.extended_data).to include(user_extended_data)
+        expect(user.extended_data.keys.count).to eq(1)
+
+        subject.perform
+        user.reload
+        expect(user.extended_data).to include(user_extended_data, "data_authorization_handler" => data_authorization.metadata)
+        expect(user.extended_data.keys.count).to eq(2)
+      end
+
+      context "and metadata already contains AH metadata" do
+        let(:user_extended_data) do
+          {
+            "extended_socio_demographic_authorization_handler" => {},
+            "data_authorization_handler" => {}
+          }
+        end
+
+        it "skip this user" do
+          expect do
+            subject.perform
+            user.reload
+          end.not_to change(user, :extended_data)
+        end
+      end
+
+      context "and metadata contains legacy format" do
+        let(:user_extended_data) do
+          {
+            "socio_email" => "example@decidim.org",
+            "socio_postal_code" => "75002",
+            "socio_city" => "Paris",
+            "socio_phone_number" => "01234567889"
+          }
+        end
+
+        it "duplicates non existing authorization metadata" do
+          subject.perform
+          user.reload
+          expect(user.extended_data).to include("extended_socio_demographic_authorization_handler" => socio_authorization.metadata)
+        end
+
+        it "removes legacy metadata" do
+          expect do
+            subject.perform
+            user.reload
+          end.to change(user, :extended_data)
+            .from(user_extended_data)
+            .to({
+                  "data_authorization_handler" => data_authorization.metadata,
+                  "extended_socio_demographic_authorization_handler" => socio_authorization.metadata
+                })
+        end
+      end
     end
-
-    it_behaves_like "has duplicated encrypted metadata to user registration_metadata"
-  end
-
-  context "when user has metadata" do
-    let!(:user) { create(:user, organization: organization) }
-    let(:user_extended_data) do
-      { "extended_socio_demographic_authorization_handler" =>
-                                   { "email" => user.email, "postal_code" => "12345",
-                                     "city" => "Barcelona", "phone_number" => "01234567889" } }
-    end
-
-    before do
-      user.update!(extended_data: user_extended_data)
-
-      expect(user.extended_data["extended_socio_demographic_authorization_handler"]).to eq(user_extended_data["extended_socio_demographic_authorization_handler"])
-      expect(user.extended_data["data_authorization_handler"]).to be_nil
-    end
-
-    it_behaves_like "has duplicated encrypted metadata to user registration_metadata"
-  end
-
-  context "when user has already metadata for socio but the old format" do
-    let!(:user) { create(:user, organization: organization) }
-
-    before do
-      user.update!(extended_data: {
-                     "socio_email" => socio_authorization.metadata["email"],
-                     "socio_postal_code" => socio_authorization.metadata["postal_code"],
-                     "socio_city" => socio_authorization.metadata["city"],
-                     "socio_phone_number" => socio_authorization.metadata["phone_number"]
-                   })
-    end
-
-    it_behaves_like "has duplicated encrypted metadata to user registration_metadata"
-  end
-
-  context "when user has already metadata for socio and data" do
-    let!(:user) { create(:user, organization: organization) }
-
-    before do
-      user.update!(extended_data: {
-                     "extended_socio_demographic_authorization_handler" => socio_authorization.metadata,
-                     "data_authorization_handler" => data_authorization.metadata
-                   })
-    end
-
-    it_behaves_like "has duplicated encrypted metadata to user registration_metadata"
   end
 end
