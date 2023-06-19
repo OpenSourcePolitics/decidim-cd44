e6742b719d96bae44c7d4c01aeb7fb422eda9b8d
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-06-19 18:32:28 +0200

    Refactor migrations tools (#329)
    # Conflicts:
    #	config/application.rb
    #	spec/lib/tasks/scaleway/storage/migrate_from_local_task_spec.rb

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module DevelopmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = "Europe/Paris"
+    config.time_zone = "Europe/Paris" unless Rails.env.test?
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
 
     # This needs to be set for correct images URLs in emails
@@ -45,8 +45,12 @@ module DevelopmentApp
     end
 
     config.after_initialize do
-      require "extends/controllers/decidim/editor_images_controller_extends"
       require "extends/controllers/decidim/devise/sessions_controller_extends"
+      require "extends/controllers/decidim/editor_images_controller_extends"
+
+      Decidim::GraphiQL::Rails.config.tap do |config|
+        config.initial_query = "{\n  deployment {\n    version\n    branch\n    remote\n    upToDate\n    currentCommit\n    latestCommit\n    locallyModified\n  }\n}".html_safe
+      end
     end
   end
 end
