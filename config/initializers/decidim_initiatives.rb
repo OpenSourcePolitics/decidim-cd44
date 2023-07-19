905fc868f622c98322a57ddf5a0bfa4215e2b2f9
armandfardeau <fardeauarmand@gmail.com>
2023-07-19 16:03:43 +0200

    Revert "backport create initiative and signature type (#46)"
    This reverts commit 5f03b4ae6e3b51b71aaf5eb34adb5d354d569f1a.

--- config/initializers/decidim_initiatives.rb
+++ config/initializers/decidim_initiatives.rb
@@ -38,6 +38,25 @@ if defined?(Decidim::Initiatives)
     config.online_voting_allowed = true
   end
 
+  Decidim::Initiatives::AdminEngine.routes do
+    resources :initiatives_settings, only: [:edit, :update], controller: "initiatives_settings"
+  end
+
+  Decidim.menu :admin_initiatives_menu do |menu|
+    menu.add_item :initiatives_settings,
+                  I18n.t("menu.initiatives_settings", scope: "decidim.admin"),
+                  decidim_admin_initiatives.edit_initiatives_setting_path(
+                    Decidim::InitiativesSettings.find_or_create_by!(
+                      organization: current_organization
+                    )
+                  ),
+                  active: is_active_link?(
+                    decidim_admin_initiatives.edit_initiatives_setting_path(
+                      Decidim::InitiativesSettings.find_or_create_by!(organization: current_organization)
+                    )
+                  ),
+                  if: allowed_to?(:update, :initiatives_settings)
+  end
   Decidim.resource_registry.find(:initiatives_type).actions += ["create"]
 
   Decidim::Initiatives::Engine.routes do
