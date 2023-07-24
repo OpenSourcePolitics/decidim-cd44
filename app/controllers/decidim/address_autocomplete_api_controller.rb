d7073db1415f105898503400caa5c71995715272
quentinchampenois <quentin.champenois7@gmail.com>
2023-07-24 18:31:41 +0200

    fix: Data authorization spec

--- app/controllers/decidim/address_autocomplete_api_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Decidim
-  class AddressAutocompleteApiController < DecidimController
-    def postal_code_autocomplete
-      render json: Decidim::MunicipalityNamesService.for(params[:postal_code])
-    end
-  end
-end
