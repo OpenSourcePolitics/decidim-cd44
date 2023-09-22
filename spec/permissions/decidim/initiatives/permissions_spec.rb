4a1ddb79df824642be14a08e19a311c577c14b5c
paulinebessoles <pauline.bessoles@opensourcepolitics.eu>
2023-09-22 14:38:50 +0200

    Lint code

--- spec/permissions/decidim/initiatives/permissions_spec.rb
+++ spec/permissions/decidim/initiatives/permissions_spec.rb
@@ -206,7 +206,7 @@ describe Decidim::Initiatives::Permissions do
       before do
         allow(Decidim::Initiatives)
           .to receive(:creation_enabled)
-                .and_return(true)
+          .and_return(true)
       end
 
       it { is_expected.to be false }
@@ -215,7 +215,7 @@ describe Decidim::Initiatives::Permissions do
         before do
           allow(Decidim::Initiatives)
             .to receive(:do_not_require_authorization)
-                  .and_return(true)
+            .and_return(true)
         end
 
         it { is_expected.to be true }
@@ -242,7 +242,7 @@ describe Decidim::Initiatives::Permissions do
       before do
         allow(Decidim::Initiatives)
           .to receive(:creation_enabled)
-                .and_return(false)
+          .and_return(false)
       end
 
       it { is_expected.to be false }
@@ -362,7 +362,7 @@ describe Decidim::Initiatives::Permissions do
           before do
             allow(Decidim::Initiatives)
               .to receive(:do_not_require_authorization)
-                    .and_return(true)
+              .and_return(true)
           end
 
           it { is_expected.to be true }
