0d1e49033feeac71768fd1c4f0a26b15ef628fb5
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-09-25 18:33:13 +0200

    fix: Prevent multiple run on migrations

--- db/migrate/20230823155806_create_decidim_initiatives_settings.decidim_initiatives.rb
+++ db/migrate/20230823155806_create_decidim_initiatives_settings.decidim_initiatives.rb
@@ -3,7 +3,7 @@
 
 class CreateDecidimInitiativesSettings < ActiveRecord::Migration[5.2]
   def change
-    create_table :decidim_initiatives_settings do |t|
+    create_table :decidim_initiatives_settings, if_not_exists: true do |t|
       t.string :initiatives_order, default: "random"
       t.references :decidim_organization, foreign_key: true, index: true
     end
