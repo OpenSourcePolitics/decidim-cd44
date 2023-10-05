0ef8b23283c56b27df6fe22e8ff8ff7533f8e108
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-10-05 16:44:16 +0200

    fix: Enable question_captcha by default

--- config/initializers/decidim_questions.rb
+++ config/initializers/decidim_questions.rb
@@ -5,8 +5,12 @@ if defined?(Decidim::QuestionCaptcha)
     if Rails.application.secrets.question_captcha[:host].present?
       config.api_endpoint = "https://#{Rails.application.secrets.question_captcha[:host]}/"
     else
-      config.perform_textcaptcha = false
       Rails.logger.warn("You must define the env var 'QUESTION_CAPTCHA_HOST' to activate question captcha")
+    end
+
+    unless Rails.application.secrets.question_captcha[:enabled]
+      config.perform_textcaptcha = false
+      Rails.logger.warn("You must define the env var 'ENABLE_QUESTION_CAPTCHA' to activate question captcha")
     end
   end
 end
